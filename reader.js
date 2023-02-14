be1f9819a9bf934b1eee726daba20ad94698fcc8
Dlunhappy226 <76965487+Dlunhappy226@users.noreply.github.com>
2023-02-13 17:54:41 -0800

    making loading screen

--- reader.js
+++ reader.js
@@ -2,21 +2,24 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const book = urlParams.get("b")
+const book = urlParams.get("b");
 
 if (book == null || book == ""){
     window.location.href = "?b=1";
 }else{
     $.getJSON("bible/t_asv.json", (data) => {
+        const startTime = Date.now();
+        $("#reader").html("");
         $.each(data.resultset.row, (key, x) => {
             if (x.field[1] == book){
-                const verse = x.field[2]+":"+x.field[3]
-                $("#reader").append("<p id='"+verse+"'><a href='#"+verse+"'>"+"["+verse+"]</a> "+x.field[4]+"</p>")
+                const verse = x.field[2]+":"+x.field[3];
+                $("#reader").append("<p id='"+verse+"'><a href='#"+verse+"'>"+"["+verse+"]</a> "+x.field[4]+"</p>");
             }
         });
         if (window.location.hash != ""){
             window.location = window.location.hash;
         }
+        console.log("Done loading ("+Math.floor((Date.now()-startTime)/1000)+" sec)")
     });
 }
 
